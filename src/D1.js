4390ddd466646b230cb0a100d158d52d39bf266f
Sunanditamody <1928136@kiit.ac.in>
2022-06-05 11:20:15 +0530

    list

---
+++ src/D1.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function D1() {
+  return (
+    <div>D1
+
+
+        <li>
+            A
+        </li>
+        <li>
+            B
+        </li>
+        <li>
+            C
+        </li>
+    </div>
+  )
+}
